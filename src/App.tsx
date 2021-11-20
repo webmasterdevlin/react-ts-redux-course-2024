3216f7042e33c4afb2163d51cc6e718dc319b553
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-11-20 14:44:31 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import { Container, CssBaseline } from "@material-ui/core";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
 import EagerRoutes from "./EagerRoutes";
 import LazyRoutes from "./LazyRoutes";
+import { configureAppStore } from "./store/configureStore";
+
+export const store = configureAppStore();
 
 function App() {
   return (
-    <CssBaseline>
-      <BrowserRouter>
-        <>
-          <NavigationBar />
-          <Container>
-            <LazyRoutes />
-          </Container>
-        </>
-      </BrowserRouter>
-    </CssBaseline>
+    <Provider store={store}>
+      <CssBaseline>
+        <BrowserRouter>
+          <>
+            <NavigationBar />
+            <Container>
+              <LazyRoutes />
+            </Container>
+          </>
+        </BrowserRouter>
+      </CssBaseline>
+    </Provider>
   );
 }
 
