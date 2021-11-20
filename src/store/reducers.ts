b01b102d342c22223019b1fcf41f075d7f6764bc
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-11-20 14:25:16 +0100

    update

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,3 +1,16 @@
 import { combineReducers } from "redux";
 
 import heroReducer from "../features/heroes/heroSlice";
+
+const injectedReducers = {
+  hero: heroReducer,
+  // more reducers to come
+};
+
+const rootReducer = combineReducers({
+  ...injectedReducers,
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export const createReducer = () => rootReducer;
