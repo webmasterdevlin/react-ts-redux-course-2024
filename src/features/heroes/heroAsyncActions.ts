d5227d2ee31d4a7a7efad06ffcbe449156d8f878
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-11-20 14:05:09 +0100

    update

--- src/features/heroes/heroAsyncActions.ts
+++ src/features/heroes/heroAsyncActions.ts
@@ -1,10 +1,14 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { EndPoints } from "../../axios/api-config";
+import { getAxios } from "../../axios/generic-api-calls";
 import { HeroActionTypes, HeroModel } from "./heroTypes";
 
 export const getHeroesAction = createAsyncThunk(
   HeroActionTypes.FETCH_HEROES,
-  () => {
+  async () => {
     // HTTP CALLS
+    const response = await getAxios<HeroModel[]>(EndPoints.heroes);
     // Return the response
+    return response.data;
   }
 );
