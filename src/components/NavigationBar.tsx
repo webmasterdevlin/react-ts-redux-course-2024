47275730e9a5db4ebcc14f128b221a22c40bd582
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-11-20 11:15:06 +0100

    update

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,16 +1,62 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  createStyles,
+  makeStyles,
+  Toolbar,
+} from "@material-ui/core";
 import { useNavigate } from "react-router-dom";
 import { pathNames } from "../LazyRoutes";
 
 const NavigationBar = () => {
   const navigate = useNavigate();
+  const classes = useStyles();
 
   return (
-    <div>
-      <button onClick={() => navigate(pathNames.home)}>HOME</button>
-      <button onClick={() => navigate(pathNames.heroes)}>HEROES</button>
-      <button onClick={() => navigate(pathNames.villains)}>VILLAINS</button>
-    </div>
+    <AppBar position="static" style={{ marginBottom: "2rem" }}>
+      <Toolbar>
+        <Box>
+          <Button
+            className={classes.button}
+            color="inherit"
+            onClick={() => navigate(pathNames.home)}
+          >
+            HOME
+          </Button>
+        </Box>
+        <Box>
+          <Button
+            className={classes.button}
+            color="inherit"
+            onClick={() => navigate(pathNames.heroes)}
+          >
+            HEROES
+          </Button>
+        </Box>
+        <Box>
+          <Button
+            className={classes.button}
+            color="inherit"
+            onClick={() => navigate(pathNames.villains)}
+          >
+            VILLAINS
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
 export default NavigationBar;
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    button: {
+      margin: "0 0.5rem",
+      "&:focus": {
+        outline: "none",
+      },
+    },
+  })
+);
