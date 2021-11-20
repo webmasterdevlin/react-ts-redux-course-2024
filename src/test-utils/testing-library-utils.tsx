a9e4c486ef36c127ed09751e52cbbb3dacb05c6d
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-11-20 11:37:43 +0100

    update

--- src/test-utils/testing-library-utils.tsx
+++ src/test-utils/testing-library-utils.tsx
@@ -3,13 +3,10 @@ import { BrowserRouter } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { Container } from "@material-ui/core";
 import NavigationBar from "../components/NavigationBar";
+import { ReactElement } from "react";
 
-type Props = {
-  children?: React.ReactNode;
-};
-
-const render = (ui, { ...renderOptions } = {}) => {
-  const Wrapper = ({ children }: Props) => (
+const render = (ui: ReactElement, { ...renderOptions } = {}) => {
+  const Wrapper = ({ children }: any) => (
     <CssBaseline>
       <BrowserRouter>
         <>
