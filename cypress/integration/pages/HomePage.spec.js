ae2e0408cf0a8736bf8bcc0dcd2e337093c899bc
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-11-20 13:05:52 +0100

    update

--- cypress/integration/pages/HomePage.spec.js
+++ cypress/integration/pages/HomePage.spec.js
@@ -1,6 +1,15 @@
-it("Should display Instagram page", () => {
-  cy.visit("https://www.instagram.com/");
-  cy.contains("Accept All").click();
-  cy.contains("Accept All").should("not.exist");
-  cy.contains("username").type("devlin.duldulao@inmeta.no");
+
+
+
+
+
+describe.skip("Instagram", () => {
+  beforeEach(() => {
+    cy.visit("https://instagram.com");
+  });
+  it("Should display Instagram page", () => {
+    cy.contains("Accept All").click();
+    cy.contains("Accept All").should("not.exist");
+    cy.contains("username").type("devlin.duldulao@inmeta.no");
+  });
 });
