1acf9a5a62f0aa6c3d44c6cc21028973dadfb578
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-11-20 11:19:55 +0100

    update

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,20 @@
+import { Box, Typography } from "@material-ui/core";
 import React from "react";
 
 const HomePage = () => {
-  return <h1>HomePage Works!</h1>;
+  return (
+    <Box
+      width={"100%"}
+      display={"flex"}
+      flex={"row"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <Typography variant={"h2"}>
+        Welcome to Redux Toolkit Course 🧑‍🏫 💻
+      </Typography>
+    </Box>
+  );
 };
 
 export default HomePage;
