1f2243fd9307d66aa6643977c886668093ad8db1
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-11-21 10:47:42 +0100

    update

--- src/pages/tests/HeroesPage.test.tsx
+++ src/pages/tests/HeroesPage.test.tsx
@@ -1,3 +1,4 @@
+import userEvent from "@testing-library/user-event";
 import { render, screen } from "../../test-utils/testing-library-utils";
 import HeroesPage from "../HeroesPage";
 
@@ -7,5 +8,23 @@ describe("Heroes Page", () => {
 
     const title = screen.getByTestId("title-page");
     expect(title).toBeInTheDocument();
+  });
+
+  it("should render loading message", () => {
+    render(<HeroesPage />);
+
+    const loading = screen.getByTestId("loading");
+    expect(loading).toHaveTextContent(/loading.. please wait../i);
+  });
+
+  it("should mark a hero", async () => {
+    render(<HeroesPage />);
+
+    const buttons = await screen.findAllByTestId("mark-button");
+    expect(buttons).toHaveLength(2);
+
+    userEvent.click(buttons[0]);
+    const cards = await screen.findAllByTestId("card");
+    expect(cards[0]).toHaveTextContent("marked");
   });
 });
